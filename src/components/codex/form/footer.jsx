d6de8a47c218d3e1e2f8eba3b38fb660a79496d8
Gijsjan <agijsbro@gmail.com>
2015-10-26 16:24:37 +0100

    Add creation/modifiation dates to edit footer

--- src/components/codex/form/footer.jsx
+++ src/components/codex/form/footer.jsx
@@ -7,9 +7,16 @@ class EditFooter extends React.Component {
 	}
 
 	render() {
+		let codex = this.props.codices.current;
+
 		return (
 			<footer>
 				<button className="cancel"><Link to={`/codex/${this.props.params.id}`}>Cancel</Link></button>
+				<div className="dates">
+					<span>Create by {codex.creator} on {codex.creationDate}</span>
+					<br />
+					<span>Modified by {codex.modifier} on {codex.modificationDate}</span>
+				</div>
 				<button className="delete" onClick={this.props.onRemoveCodex}>Delete</button>
 				<button className="save" onClick={this.props.onSave}>Save</button>
 			</footer>

58f0eae56a0ecb67a0874569e24f478657a28e37
Gijsjan <agijsbro@gmail.com>
2016-03-29 12:04:35 +0200

    Add authors to text unit

--- src/components/codex/record/text-unit.jsx
+++ src/components/codex/record/text-unit.jsx
@@ -3,6 +3,11 @@ import {Tabs, Tab} from "hire-tabs";
 import Text from "./elements/text";
 import Well from "../../well";
 
+const flatten = (prev, curr) => prev.concat(curr);
+
+const unique = (prev, curr) =>
+	(prev.indexOf(curr) === -1) ? prev.concat(curr) : prev
+
 class TextUnit extends React.Component {
 	constructor(props) {
 		super(props);
@@ -14,7 +19,16 @@ class TextUnit extends React.Component {
 
 	render() {
 		let codex = this.props.codices.current;
-
+		// console.log(codex)
+		// const authors = codex.textUnits
+		// 	.map((textUnit) =>
+		// 		textUnit.text.authors.map((author) =>
+		// 			`<li>${author.person.value}</li>`
+		// 		)
+		// 	)
+		// 	// .reduce(flatten)
+		// console.log(authors.reduce, flatten)
+		// console.log("fl", authors.reduce(flatten, []))
 		return (
 			<Tabs
 				className="sub-menu"
@@ -30,7 +44,19 @@ class TextUnit extends React.Component {
 							<Text label="Incipit">{textUnit.incipit}</Text>
 							<Text label="Explicit">{textUnit.explicit}</Text>
 							<Text label="Page range">{textUnit.pages}</Text>
-							<Text label="Author(s)">{"authors"}</Text>
+							<div className="list">
+								<label>Author(s)</label>
+								<ul>{
+									codex.textUnits
+										.map((textUnit) =>
+											textUnit.text.authors.map((author) =>
+												<li>{author.person.value}</li>
+											)
+										)
+										.reduce(flatten, [])
+										.reduce(unique, [])
+								}</ul>
+						</div>
 							<Text label="Period">{textUnit.text.period}</Text>
 							<Text label="Language">{textUnit.text.language}</Text>
 							<Text label="Genre">{textUnit.contentTypes}</Text>

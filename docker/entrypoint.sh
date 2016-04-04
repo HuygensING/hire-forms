1a55a01e1025e2eb02e922ce3a0b17106f964025
Gijsjan <agijsbro@gmail.com>
2016-04-04 08:28:20 +0000

    Add docker env

---
+++ docker/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# NPM link hilib
+cd ./hire-login
+echo "developer" | sudo -S npm link
+
+cd ../hire-faceted-search
+echo "developer" | sudo -S npm link
+
+ssh-add ~/.ssh/id_rsa
+
+# Start tmuxinator
+mux marginal-scholarship
